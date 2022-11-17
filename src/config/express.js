eec34e6cfa76f4a80febe7c3c48ab78a1c151af3
HarryChenTW <whchen0630@gmail.com>
2022-11-17 17:06:27 +0800

    update code

--- src/config/express.js
+++ src/config/express.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
-const morgan = require('morgan');
 const config = require('./config');
 const index = require('../server/routes/index.route');
 
@@ -17,11 +15,5 @@ app.use('/api', index);
 // parse body params and attache them to req.body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// enable CORS - Cross Origin Resource Sharing
-app.use(cors());
-
-// HTTP request logger middleware for node.js
-app.use(morgan('dev'));
 
 module.exports = app;

5f301fd1e8a3a112ff267642a42b17748ed2bdce
HarryChenTW <whchen0630@gmail.com>
2022-12-03 16:54:03 +0800

    separate league's route

--- src/server/controllers/games.controller.js
+++ src/server/controllers/games.controller.js
@@ -1,6 +1,6 @@
 const gamesModule = require('../modules/games.module');
 
-const getGames = async (req, res) => {
+const getAllGames = async (req, res) => {
     const pGames = await gamesModule.getPLeagueThisWeek().catch(() => res.send('Error 500 : failed fetching data from database'));
     const t1Games = await gamesModule.getT1LeagueThisWeek().catch(() => res.send('Error 500 : failed fetching data from database'));
     const games = [pGames, t1Games];
@@ -23,4 +23,40 @@ const getGames = async (req, res) => {
     res.send(sortedGames);
 };
 
-module.exports = getGames;
+const getPleague = async (req, res) => {
+    const pGames = await gamesModule.getPLeagueThisWeek().catch(() => res.send('Error 500 : failed fetching data from database'));
+
+    // sort by date
+    const sortedGames = await pGames.sort((a, b) => {
+        const aDatetime = new Date(a.datetime);
+        const bDatatime = new Date(b.datetime);
+        return aDatetime - bDatatime;
+    });
+
+    // convert timezone of date (for more readable)
+    for (let i = 0; i < sortedGames.length; i += 1) {
+        sortedGames[i].datetime = new Date(sortedGames[i].datetime).toLocaleString('en-US', { timeZone: 'Asia/Taipei' });
+    }
+
+    res.send(sortedGames);
+};
+
+const getT1league = async (req, res) => {
+    const t1Games = await gamesModule.getT1LeagueThisWeek().catch(() => res.send('Error 500 : failed fetching data from database'));
+
+    // sort by date
+    const sortedGames = await t1Games.sort((a, b) => {
+        const aDatetime = new Date(a.datetime);
+        const bDatatime = new Date(b.datetime);
+        return aDatetime - bDatatime;
+    });
+
+    // convert timezone of date (for more readable)
+    for (let i = 0; i < sortedGames.length; i += 1) {
+        sortedGames[i].datetime = new Date(sortedGames[i].datetime).toLocaleString('en-US', { timeZone: 'Asia/Taipei' });
+    }
+
+    res.send(sortedGames);
+};
+
+module.exports = { getAllGames, getPleague, getT1league };
--- src/server/routes/games.route.js
+++ src/server/routes/games.route.js
@@ -4,6 +4,10 @@ const gamesCtrl = require('../controllers/games.controller');
 const router = express.Router();
 
 /* get games homepage */
-router.get('/', gamesCtrl);
+router.get('/', gamesCtrl.getAllGames);
+
+/* separate league */
+router.get('/pleague', gamesCtrl.getPleague);
+router.get('/t1league', gamesCtrl.getT1league);
 
 module.exports = router;

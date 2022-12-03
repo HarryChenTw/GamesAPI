24b67b4cf85542d6def1b40a2cd69782f638308b
HarryChenTW <whchen0630@gmail.com>
2022-12-03 20:00:56 +0800

    edit API description

--- src/server/routes/index.route.js
+++ src/server/routes/index.route.js
@@ -12,6 +12,7 @@ router.get('/', (req, res) => {
         <ul>\
             <li><b> .../api/games/pleague </b>  → get upcoming games from P League </li>\
             <li><b> .../api/games/t1league </b>  → get upcoming games from T1 League </li>\
+            <li><b> .../api/games/uba </b>  → get upcoming games from UBA </li>\
         </ul>\
     </ul>\
     ';
